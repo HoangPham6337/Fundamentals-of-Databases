0c71f5fbaa71d4315f859bb147c91be3132e21ae
HoangPham4171 <52208311+HoangPham6337@users.noreply.github.com>
2023-11-28 23:20:55 +0700

    Adding DEFAULT states

--- Pharmacy.sql
+++ Pharmacy.sql
@@ -5,76 +5,76 @@ USE Pharmacy;
 -- Create the Manufacturer table
 CREATE TABLE Manufacturer (
     ManufacturerID VARCHAR(4) PRIMARY KEY NOT NULL,
-    CompanyName VARCHAR(50),
-    Address VARCHAR(100),
-    PhoneNumber VARCHAR(15),
-    Website VARCHAR(50)
+    CompanyName VARCHAR(50) DEFAULT NULL,
+    Address VARCHAR(100) DEFAULT NULL,
+    PhoneNumber VARCHAR(15) DEFAULT NULL,
+    Website VARCHAR(50) DEFAULT NULL
 );
 
 -- Create the Supplier table
 CREATE TABLE Supplier (
     SupplierID VARCHAR(4) PRIMARY KEY NOT NULL,
-    CompanyName VARCHAR(50),
-    HeadOffice VARCHAR(100),
-    Website VARCHAR(50),
-    Hotline VARCHAR(15)
+    CompanyName VARCHAR(50) DEFAULT NULL,
+    HeadOffice VARCHAR(100) DEFAULT NULL,
+    Website VARCHAR(50) DEFAULT NULL,
+    Hotline VARCHAR(15) DEFAULT NULL
 );
 
 -- Create the BatchDetails table
 CREATE TABLE BatchDetails (
     BatchNumber VARCHAR(20) PRIMARY KEY NOT NULL,
     SupplierID VARCHAR(4) NOT NULL,
-    ShippingDatetime DATE,
-    DeparturePort VARCHAR(20),
-    DestinationPort VARCHAR(20),
+    ShippingDatetime DATE DEFAULT NULL,
+    DeparturePort VARCHAR(20) DEFAULT NULL,
+    DestinationPort VARCHAR(20) DEFAULT NULL,
     Foreign Key (SupplierID) REFERENCES Supplier(SupplierID)
 );
 
 -- Create the Drug table
 CREATE TABLE Drugs(
     DrugID VARCHAR(4) PRIMARY KEY NOT NULL,
-    DrugName VARCHAR(50) NOT NULL,
+    DrugName VARCHAR(50) DEFAULT NULL,
     BatchNumber VARCHAR(20) NOT NULL,
-    ExpiryDate DATE,
-    StorageCondition VARCHAR(20),
-    ArrivalDate DATE,
-    Quantity INT,
-    PurchasePrice DECIMAL(10, 2),
-    SellingPrice DECIMAL(10, 2),
-    ManufacturerID VARCHAR(4),
-    Foreign KEY (ManufacturerID) REFERENCES Manufacturer(ManufacturerID),
+    ExpiryDate DATE DEFAULT NULL,
+    StorageCondition VARCHAR(20) DEFAULT NULL,
+    ArrivalDate DATE DEFAULT NULL,
+    Quantity INT DEFAULT NULL,
+    PurchasePrice DECIMAL(10, 2) DEFAULT NULL,
+    SellingPrice DECIMAL(10, 2) DEFAULT NULL,
+    ManufacturerID VARCHAR(4) NOT NULL,
+    FOREIGN KEY (ManufacturerID) REFERENCES Manufacturer(ManufacturerID),
     FOREIGN KEY (BatchNumber) REFERENCES BatchDetails(BatchNumber)
 );
 
 -- Create the Customer table
 CREATE TABLE Customer (
     CustomerID VARCHAR(4) PRIMARY KEY NOT NULL,
-    FirstName VARCHAR(50),
-    LastName VARCHAR(50),
-    Phone VARCHAR(15),
-    Address VARCHAR(100),
-    Email VARCHAR(30)
+    FirstName VARCHAR(50) DEFAULT NULL,
+    LastName VARCHAR(50) DEFAULT NULL,
+    Phone VARCHAR(15) DEFAULT NULL,
+    Address VARCHAR(100) DEFAULT NULL,
+    Email VARCHAR(30) DEFAULT NULL
 );
 
 -- Create the Staff table
 CREATE TABLE Staff (
     StaffID VARCHAR(4) PRIMARY KEY NOT NULL,
-    FirstName VARCHAR(50),
-    LastName VARCHAR(50),
-    HireDate DATE,
-    Phone VARCHAR(15),
-    Address VARCHAR(100),
-    Email VARCHAR(100)
+    FirstName VARCHAR(50) DEFAULT NULL,
+    LastName VARCHAR(50) DEFAULT NULL,
+    HireDate DATE DEFAULT NULL,
+    Phone VARCHAR(15) DEFAULT NULL,
+    Address VARCHAR(100) DEFAULT NULL,
+    Email VARCHAR(100) DEFAULT NULL
 );
 
 -- Create the Shipper table
 CREATE TABLE Shipper (
     ShipperID VARCHAR(4) PRIMARY KEY NOT NULL,
-    FirstName VARCHAR(50),
-    LastName VARCHAR(50),
-    Phone VARCHAR(15),
-    Address VARCHAR(100),
-    Email VARCHAR(100)
+    FirstName VARCHAR(50) DEFAULT NULL,
+    LastName VARCHAR(50) DEFAULT NULL,
+    Phone VARCHAR(15) DEFAULT NULL,
+    Address VARCHAR(100) DEFAULT NULL,
+    Email VARCHAR(100) DEFAULT NULL
 );
 
 CREATE TABLE Orders (
@@ -82,9 +82,9 @@ CREATE TABLE Orders (
     CustomerID VARCHAR(4) NOT NULL,
     StaffID VARCHAR(4) NOT NULL,
     ShipperID VARCHAR(4) NOT NULL,
-    Quantity INT,
-    OrderDate DATE,
-    Status VARCHAR(20),
+    Quantity INT DEFAULT 0,
+    OrderDate DATE DEFAULT CURRENT_Date,
+    Status VARCHAR(20) DEFAULT 'Pending',
     Foreign Key (DrugID) REFERENCES Drugs(DrugID),
     Foreign Key (CustomerID) REFERENCES Drugs(DrugID),
     Foreign Key (StaffID) REFERENCES Staff(StaffID),
--- query.sql
+++ query.sql
@@ -5,3 +5,6 @@ SELECT * FROM `Manufacturer`;
 SELECT * FROM `Shipper`;
 SELECT * FROM `Staff`;
 SELECT * FROM `Supplier`;
+SELECT * FROM `Orders`;
+
+INSERT INTO Orders() VALUES()
