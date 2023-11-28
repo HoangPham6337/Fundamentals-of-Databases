741cbf91e1d593fc8bb6429914a6458a37256d9f
HoangPham4171 <52208311+HoangPham6337@users.noreply.github.com>
2023-11-28 22:50:15 +0700

    Add Primary Key and Relations

--- Pharmacy.sql
+++ Pharmacy.sql
@@ -2,9 +2,37 @@ DROP DATABASE IF EXISTS Pharmacy;
 CREATE DATABASE IF NOT EXISTS Pharmacy;
 USE Pharmacy;
 
+-- Create the Manufacturer table
+CREATE TABLE Manufacturer (
+    ManufacturerID VARCHAR(4) PRIMARY KEY NOT NULL,
+    CompanyName VARCHAR(50),
+    Address VARCHAR(100),
+    PhoneNumber VARCHAR(15),
+    Website VARCHAR(50)
+);
+
+-- Create the Supplier table
+CREATE TABLE Supplier (
+    SupplierID VARCHAR(4) PRIMARY KEY NOT NULL,
+    CompanyName VARCHAR(50),
+    HeadOffice VARCHAR(100),
+    Website VARCHAR(50),
+    Hotline VARCHAR(15)
+);
+
+-- Create the BatchDetails table
+CREATE TABLE BatchDetails (
+    BatchNumber VARCHAR(20) PRIMARY KEY NOT NULL,
+    SupplierID VARCHAR(4) NOT NULL,
+    ShippingDatetime DATE,
+    DeparturePort VARCHAR(20),
+    DestinationPort VARCHAR(20),
+    Foreign Key (SupplierID) REFERENCES Supplier(SupplierID)
+);
+
 -- Create the Drug table
 CREATE TABLE Drugs(
-    DrugID VARCHAR(4) NOT NULL,
+    DrugID VARCHAR(4) PRIMARY KEY NOT NULL,
     DrugName VARCHAR(50) NOT NULL,
     BatchNumber VARCHAR(20) NOT NULL,
     ExpiryDate DATE,
@@ -13,30 +41,14 @@ CREATE TABLE Drugs(
     Quantity INT,
     PurchasePrice DECIMAL(10, 2),
     SellingPrice DECIMAL(10, 2),
-    ManufacturerID VARCHAR(4)
-);
-
--- Create the Manufacturer table
-CREATE TABLE Manufacturer (
-    ManufacturerID VARCHAR(4) NOT NULL,
-    CompanyName VARCHAR(50) NOT NULL,
-    Address VARCHAR(100),
-    PhoneNumber VARCHAR(15),
-    Website VARCHAR(50)
-);
-
--- Create the BatchDetails table
-CREATE TABLE BatchDetails (
-    BatchNumber VARCHAR(20) NOT NULL,
-    SupplierID VARCHAR(4) NOT NULL,
-    ShippingDatetime DATE,
-    DeparturePort VARCHAR(20),
-    DestinationPort VARCHAR(20)
+    ManufacturerID VARCHAR(4),
+    Foreign KEY (ManufacturerID) REFERENCES Manufacturer(ManufacturerID),
+    FOREIGN KEY (BatchNumber) REFERENCES BatchDetails(BatchNumber)
 );
 
 -- Create the Customer table
 CREATE TABLE Customer (
-    CustomerID VARCHAR(4) NOT NULL,
+    CustomerID VARCHAR(4) PRIMARY KEY NOT NULL,
     FirstName VARCHAR(50),
     LastName VARCHAR(50),
     Phone VARCHAR(15),
@@ -46,7 +58,7 @@ CREATE TABLE Customer (
 
 -- Create the Staff table
 CREATE TABLE Staff (
-    StaffID VARCHAR(4) NOT NULL,
+    StaffID VARCHAR(4) PRIMARY KEY NOT NULL,
     FirstName VARCHAR(50),
     LastName VARCHAR(50),
     HireDate DATE,
@@ -55,21 +67,26 @@ CREATE TABLE Staff (
     Email VARCHAR(100)
 );
 
--- Create the Supplier table
-CREATE TABLE Supplier (
-    SupplierID VARCHAR(4) NOT NULL,
-    CompanyName VARCHAR(50),
-    HeadOffice VARCHAR(100),
-    Website VARCHAR(50),
-    Hotline VARCHAR(15)
-);
-
 -- Create the Shipper table
 CREATE TABLE Shipper (
-    ShipperID VARCHAR(4) NOT NULL,
+    ShipperID VARCHAR(4) PRIMARY KEY NOT NULL,
     FirstName VARCHAR(50),
     LastName VARCHAR(50),
     Phone VARCHAR(15),
     Address VARCHAR(100),
     Email VARCHAR(100)
 );
+
+CREATE TABLE Orders (
+    DrugID VARCHAR(4) NOT NULL,
+    CustomerID VARCHAR(4) NOT NULL,
+    StaffID VARCHAR(4) NOT NULL,
+    ShipperID VARCHAR(4) NOT NULL,
+    Quantity INT,
+    OrderDate DATE,
+    Status VARCHAR(20),
+    Foreign Key (DrugID) REFERENCES Drugs(DrugID),
+    Foreign Key (CustomerID) REFERENCES Drugs(DrugID),
+    Foreign Key (StaffID) REFERENCES Staff(StaffID),
+    Foreign Key (ShipperID) REFERENCES Shipper(ShipperID)
+)
--- inserting_values.sql
+++ inserting_values.sql
@@ -1,25 +1,33 @@
--- Insert data into the Drugs table
-INSERT INTO Drugs VALUES
-('D001', 'Tranqoquil', 'TQ123456', '2024-08-01', 'Cool and Dry', '2023-11-20', 100, 50.00, 75.00, 'M001'),
-('D002', 'Revitalix', 'RV789012', '2024-10-15', 'Room Temperature', '2023-11-20', 150, 40.00, 60.00, 'M002'),
-('D003', 'Zenthrax', 'ZT654321', '2024-09-05', 'Refrigerated', '2023-11-20', 75, 80.00, 120.00, 'M003'),
-('D004', 'AllergiGuard', 'AG112233', '2024-07-12', 'Controlled Humidity', '2023-11-20', 200, 30.00, 45.00, 'M004'),
-('D005', 'NeuroSync', 'NS445566', '2024-11-30', 'Cool and Dry', '2023-11-20', 120, 90.00, 150.00, 'M005'),
-('D006', 'CardioRelief', 'CR778899', '2024-08-20', 'Room Temperature', '2023-05-22', 180, 65.00, 100.00, 'M006'),
-('D007', 'DermaFlex', 'DF001122', '2024-09-28', 'Controlled Humidity', '2023-05-22', 90, 55.00, 80.00, 'M007'),
-('D008', 'RespiraMax', 'RM334455', '2024-07-05', 'Refrigerated', '2023-05-22', 250, 75.00, 110.00, 'M008'),
-('D009', 'GastroShield', 'GS667788', '2024-10-10', 'Cool and Dry', '2023-05-22', 130, 40.00, 60.00, 'M009'),
-('D010', 'OsteoFortis', 'OF990011', '2024-09-15', 'Room Temperature', '2023-05-22', 160, 85.00, 130.00, 'M010'),
-('D011', 'VisionClear', 'VC223344', '2024-08-08', 'Controlled Humidity', '2023-05-22', 110, 70.00, 100.00, 'M011'),
-('D012', 'ImmunoGuard', 'IG556677', '2024-11-25', 'Refrigerated', '2023-11-01', 200, 95.00, 140.00, 'M012'),
-('D013', 'PainEase', 'PE889900', '2024-07-18', 'Cool and Dry', '2023-11-01', 80, 50.00, 75.00, 'M013'),
-('D014', 'SleepSure', 'SS112233', '2024-10-01', 'Room Temperature', '2023-11-01', 140, 45.00, 70.00, 'M014'),
-('D015', 'DigestiPro', 'DP334455', '2024-09-20', 'Controlled Humidity', '2023-11-01', 170, 60.00, 90.00, 'M015'),
-('D016', 'ClarityBlend', 'CB667788', '2024-08-12', 'Refrigerated', '2023-11-01', 120, 75.00, 110.00, 'M016'),
-('D017', 'EnergyBurst', 'EB990011', '2024-11-10', 'Cool and Dry', '2023-10-22', 190, 55.00, 85.00, 'M017'),
-('D018', 'JointFlex', 'JF223344', '2024-07-28', 'Room Temperature', '2023-10-22', 100, 65.00, 100.00, 'M018'),
-('D019', 'SkinRevive', 'SR556677', '2024-10-05', 'Controlled Humidity', '2023-10-22', 150, 80.00, 120.00, 'M019'),
-('D020', 'FocusBoost', 'FB889900', '2024-09-01', 'Refrigerated', '2023-10-22', 220, 90.00, 130.00, 'M020');
+-- Insert data into the Manufacturer table
+INSERT INTO Manufacturer VALUES
+('M001', 'Pfizer', '123 Main Street, City A', '555-123-4567', 'www.pfizer.com'),
+('M002', 'Johnson & Johnson', '456 Oak Avenue, City B', '555-234-5678', 'www.jnj.com'),
+('M003', 'Novartis', '789 Elm Lane, City C', '555-345-6789', 'www.novartis.com'),
+('M004', 'Roche', '321 Pine Road, City D', '555-456-7890', 'www.roche.com'),
+('M005', 'Merck', '567 Birch Drive, City E', '555-567-8901', 'www.merck.com'),
+('M006', 'GlaxoSmithKline', '890 Cedar Street, City F', '555-678-9012', 'www.gsk.com'),
+('M007', 'AstraZeneca', '234 Maple Avenue, City G', '555-789-0123', 'www.astrazeneca.com'),
+('M008', 'Sanofi', '567 Oak Lane, City H', '555-890-1234', 'www.sanofi.com'),
+('M009', 'Eli Lilly', '901 Pine Drive, City I', '555-901-2345', 'www.lilly.com'),
+('M010', 'AbbVie', '123 Elm Road, City J', '555-012-3456', 'www.abbvie.com'),
+('M011', 'Bristol Myers', '345 Cedar Avenue, City K', '555-123-4567', 'www.bms.com'),
+('M012', 'Bayer', '678 Maple Lane, City L', '555-234-5678', 'www.bayer.com'),
+('M013', 'Moderna', '901 Birch Street, City M', '555-345-6789', 'www.moderna.com'),
+('M014', 'Pfizer', '234 Pine Drive, City N', '555-456-7890', 'www.pfizer.com'),
+('M015', 'Johnson & Johnson', '567 Elm Avenue, City O', '555-567-8901', 'www.jnj.com'),
+('M016', 'Novartis', '890 Oak Road, City P', '555-678-9012', 'www.novartis.com'),
+('M017', 'Roche', '123 Cedar Lane, City Q', '555-789-0123', 'www.roche.com'),
+('M018', 'Merck', '345 Maple Drive, City R', '555-890-1234', 'www.merck.com'),
+('M019', 'GlaxoSmithKline', '678 Pine Avenue, City S', '555-901-2345', 'www.gsk.com'),
+('M020', 'AstraZeneca', '901 Elm Road, City T', '555-012-3456', 'www.astrazeneca.com');
+
+-- Insert data into the Supplier table
+INSERT INTO Supplier VALUES
+('S001', 'Pfizer', '123 Main Street, City A', 'www.pfizer-supplier.com', '555-123-4567'),
+('S002', 'Johnson & Johnson', '456 Oak Avenue, City B', 'www.jnj-supplier.com', '555-234-5678'),
+('S003', 'Novartis', '789 Elm Lane, City C', 'www.novartis-supplier.com', '555-345-6789'),
+('S004', 'Roche', '321 Pine Road, City D', 'www.roche-supplier.com', '555-456-7890'),
+('S005', 'Merck', '567 Birch Drive, City E', 'www.merck-supplier.com', '555-567-8901');
 
 -- Insert data into the BatchDetails table
 INSERT INTO BatchDetails VALUES
@@ -44,28 +52,28 @@ INSERT INTO BatchDetails VALUES
 ('SR556677', 'S004', '2023-10-15', 'Miami', 'New York'),
 ('FB889900', 'S005', '2023-10-15', 'Los Angeles', 'New York');
 
--- Insert data into the Manufacturer table
-INSERT INTO Manufacturer VALUES
-('M001', 'Pfizer', '123 Main Street, City A', '555-123-4567', 'www.pfizer.com'),
-('M002', 'Johnson & Johnson', '456 Oak Avenue, City B', '555-234-5678', 'www.jnj.com'),
-('M003', 'Novartis', '789 Elm Lane, City C', '555-345-6789', 'www.novartis.com'),
-('M004', 'Roche', '321 Pine Road, City D', '555-456-7890', 'www.roche.com'),
-('M005', 'Merck', '567 Birch Drive, City E', '555-567-8901', 'www.merck.com'),
-('M006', 'GlaxoSmithKline', '890 Cedar Street, City F', '555-678-9012', 'www.gsk.com'),
-('M007', 'AstraZeneca', '234 Maple Avenue, City G', '555-789-0123', 'www.astrazeneca.com'),
-('M008', 'Sanofi', '567 Oak Lane, City H', '555-890-1234', 'www.sanofi.com'),
-('M009', 'Eli Lilly', '901 Pine Drive, City I', '555-901-2345', 'www.lilly.com'),
-('M010', 'AbbVie', '123 Elm Road, City J', '555-012-3456', 'www.abbvie.com'),
-('M011', 'Bristol Myers', '345 Cedar Avenue, City K', '555-123-4567', 'www.bms.com'),
-('M012', 'Bayer', '678 Maple Lane, City L', '555-234-5678', 'www.bayer.com'),
-('M013', 'Moderna', '901 Birch Street, City M', '555-345-6789', 'www.moderna.com'),
-('M014', 'Pfizer', '234 Pine Drive, City N', '555-456-7890', 'www.pfizer.com'),
-('M015', 'Johnson & Johnson', '567 Elm Avenue, City O', '555-567-8901', 'www.jnj.com'),
-('M016', 'Novartis', '890 Oak Road, City P', '555-678-9012', 'www.novartis.com'),
-('M017', 'Roche', '123 Cedar Lane, City Q', '555-789-0123', 'www.roche.com'),
-('M018', 'Merck', '345 Maple Drive, City R', '555-890-1234', 'www.merck.com'),
-('M019', 'GlaxoSmithKline', '678 Pine Avenue, City S', '555-901-2345', 'www.gsk.com'),
-('M020', 'AstraZeneca', '901 Elm Road, City T', '555-012-3456', 'www.astrazeneca.com');
+-- Insert data into the Drugs table
+INSERT INTO Drugs VALUES
+('D001', 'Tranqoquil', 'TQ123456', '2024-08-01', 'Cool and Dry', '2023-11-20', 100, 50.00, 75.00, 'M001'),
+('D002', 'Revitalix', 'RV789012', '2024-10-15', 'Room Temperature', '2023-11-20', 150, 40.00, 60.00, 'M002'),
+('D003', 'Zenthrax', 'ZT654321', '2024-09-05', 'Refrigerated', '2023-11-20', 75, 80.00, 120.00, 'M003'),
+('D004', 'AllergiGuard', 'AG112233', '2024-07-12', 'Controlled Humidity', '2023-11-20', 200, 30.00, 45.00, 'M004'),
+('D005', 'NeuroSync', 'NS445566', '2024-11-30', 'Cool and Dry', '2023-11-20', 120, 90.00, 150.00, 'M005'),
+('D006', 'CardioRelief', 'CR778899', '2024-08-20', 'Room Temperature', '2023-05-22', 180, 65.00, 100.00, 'M006'),
+('D007', 'DermaFlex', 'DF001122', '2024-09-28', 'Controlled Humidity', '2023-05-22', 90, 55.00, 80.00, 'M007'),
+('D008', 'RespiraMax', 'RM334455', '2024-07-05', 'Refrigerated', '2023-05-22', 250, 75.00, 110.00, 'M008'),
+('D009', 'GastroShield', 'GS667788', '2024-10-10', 'Cool and Dry', '2023-05-22', 130, 40.00, 60.00, 'M009'),
+('D010', 'OsteoFortis', 'OF990011', '2024-09-15', 'Room Temperature', '2023-05-22', 160, 85.00, 130.00, 'M010'),
+('D011', 'VisionClear', 'VC223344', '2024-08-08', 'Controlled Humidity', '2023-05-22', 110, 70.00, 100.00, 'M011'),
+('D012', 'ImmunoGuard', 'IG556677', '2024-11-25', 'Refrigerated', '2023-11-01', 200, 95.00, 140.00, 'M012'),
+('D013', 'PainEase', 'PE889900', '2024-07-18', 'Cool and Dry', '2023-11-01', 80, 50.00, 75.00, 'M013'),
+('D014', 'SleepSure', 'SS112233', '2024-10-01', 'Room Temperature', '2023-11-01', 140, 45.00, 70.00, 'M014'),
+('D015', 'DigestiPro', 'DP334455', '2024-09-20', 'Controlled Humidity', '2023-11-01', 170, 60.00, 90.00, 'M015'),
+('D016', 'ClarityBlend', 'CB667788', '2024-08-12', 'Refrigerated', '2023-11-01', 120, 75.00, 110.00, 'M016'),
+('D017', 'EnergyBurst', 'EB990011', '2024-11-10', 'Cool and Dry', '2023-10-22', 190, 55.00, 85.00, 'M017'),
+('D018', 'JointFlex', 'JF223344', '2024-07-28', 'Room Temperature', '2023-10-22', 100, 65.00, 100.00, 'M018'),
+('D019', 'SkinRevive', 'SR556677', '2024-10-05', 'Controlled Humidity', '2023-10-22', 150, 80.00, 120.00, 'M019'),
+('D020', 'FocusBoost', 'FB889900', '2024-09-01', 'Refrigerated', '2023-10-22', 220, 90.00, 130.00, 'M020');
 
 -- Insert data into the Customer table
 INSERT INTO Customer VALUES
@@ -93,13 +101,6 @@ INSERT INTO Staff VALUES
 ('PS09', 'Noah', 'Martin', '2023-09-22', '555-901-2345', '901 Pine Drive, City I', 'noah.martin@emailprovider.com'),
 ('PS10', 'Sophia', 'Hall', '2023-10-10', '555-012-3456', '123 Elm Road, City J', 'sophia.hall@emailprovider.com');
 
--- Insert data into the Supplier table
-INSERT INTO Supplier VALUES
-('S001', 'Pfizer', '123 Main Street, City A', 'www.pfizer-supplier.com', '555-123-4567'),
-('S002', 'Johnson & Johnson', '456 Oak Avenue, City B', 'www.jnj-supplier.com', '555-234-5678'),
-('S003', 'Novartis', '789 Elm Lane, City C', 'www.novartis-supplier.com', '555-345-6789'),
-('S004', 'Roche', '321 Pine Road, City D', 'www.roche-supplier.com', '555-456-7890'),
-('S005', 'Merck', '567 Birch Drive, City E', 'www.merck-supplier.com', '555-567-8901');
 
 -- Insert data into the Shipper table
 INSERT INTO Shipper VALUES
