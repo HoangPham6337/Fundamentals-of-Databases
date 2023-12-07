49fe17c2f11c0fec0e2479892f2097af6d08d349
HoangPham4171 <52208311+HoangPham6337@users.noreply.github.com>
2023-12-07 23:44:14 +0700

    Move some advanced_query to its respective file

--- query/advanced_query.sql
+++ query/advanced_query.sql
@@ -27,3 +27,50 @@ FROM drugs AS a LEFT JOIN orders AS b
 ON a.`DrugID` = b.`DrugID` AND (YEAR(b.`OrderDate`) = 2022)
 GROUP BY a.`DrugID`
 ORDER BY COALESCE (SUM(b.`Quantity`), 0) DESC;
+
+-- Search for customers by address, phone number, purchased value, and preferred drug subjects
+SELECT
+    C.`CustomerID`,
+    CONCAT(C.`FirstName`, ' ', C.`LastName`) AS CustomerName,
+    C.`Customer_Address` AS Address,
+    C.`Phone` AS PhoneNumber,
+    SUM(D.`SellingPrice` * O.`Quantity`) AS PurchasedValue,
+    GROUP_CONCAT(DISTINCT D.`DrugName` ORDER BY D.`DrugName` ASC SEPARATOR ', ') AS PreferredDrugSubjects
+FROM `Customer` AS C
+JOIN `Orders` AS O ON C.`CustomerID` = O.`CustomerID`
+JOIN `Drugs` AS D ON O.`DrugID` = D.`DrugID`
+GROUP BY
+    C.`CustomerID`, C.`FirstName`, C.`LastName`, C.`Customer_Address`, C.`Phone`
+ORDER BY Address, PhoneNumber, PurchasedValue DESC;
+
+    -- Identify customers who purchase the most during a specific time frame
+SELECT
+    C.`CustomerID`,
+    CONCAT(C.`FirstName`, ' ', C.`LastName`) AS CustomerName,
+    C.`Customer_Address` AS Address,
+    C.`Phone` AS PhoneNumber,
+    SUM(D.`SellingPrice` * O.`Quantity`) AS TotalPurchasedValue
+FROM `Customer` AS C
+JOIN `Orders` AS O ON C.`CustomerID` = O.`CustomerID`
+JOIN `Drugs` AS D ON O.`DrugID` = D.`DrugID`
+WHERE O.`OrderDate` BETWEEN '2023-01-01' AND '2023-12-31' 
+GROUP BY
+    C.`CustomerID`, C.`FirstName`, C.`LastName`, C.`Customer_Address`, C.`Phone`
+ORDER BY TotalPurchasedValue DESC
+LIMIT 5;
+
+-- Identify customers who bought drugs related to the subject "Tranqoquil" and the number of drugs they purchased
+SELECT
+    C.`CustomerID`,
+    CONCAT(C.`FirstName`, ' ', C.`LastName`) AS CustomerName,
+    C.`Customer_Address` AS Address,
+    C.`Phone` AS PhoneNumber,
+    D.`DrugName` AS PurchasedDrug,
+    COUNT(*) AS NumberOfDrugsPurchased
+FROM `Customer` AS C
+JOIN `Orders` AS O ON C.`CustomerID` = O.`CustomerID`
+JOIN `Drugs` AS D ON O.`DrugID` = D.`DrugID`
+WHERE D.`DrugName` = 'Tranqoquil' 
+GROUP BY
+    C.`CustomerID`, C.`FirstName`, C.`LastName`, C.`Customer_Address`, C.`Phone`, D.`DrugName`
+ORDER BY NumberOfDrugsPurchased DESC;
